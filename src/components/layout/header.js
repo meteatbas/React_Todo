5706e4e282ee29f6903e49502fd75d0669d7fd3e
meteatbs <130202020@kocaeli.edu.tr>
2020-06-25 22:14:22 +0300

    first commit

---
+++ src/components/layout/header.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+function Header(){
+    return (
+        <header style={headerStyle}>
+            <h1>
+                Todolist
+            </h1>
+            <Link style={LinkStyle} to="/">Home</Link> | <Link style={LinkStyle} to="/about">About</Link>
+        </header>
+    )
+}
+
+
+const LinkStyle={
+    color:'#fff',
+    textDecoration:'none'
+}
+
+const headerStyle={
+    background:'#333',
+    color:'#fff',
+    textAlign:'center',
+    padding:'10px'
+}
+
+export default Header;
---
+++ src/components/pages/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+ function About() {
+    return (
+        <React.Fragment>
+            <h1>About</h1>
+            <p>V 1.0.0</p>
+        </React.Fragment>
+    )
+}
+
+
+export default About;
